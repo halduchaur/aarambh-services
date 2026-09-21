847156e839735032bd69b697443f4375ae4542e7
Gaurav Bisht <gaurav.bisht@kellton.com>
2026-09-21 21:15:53 +0530

    fix

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -42,7 +42,7 @@ const Header = function () {
                             className={language === "en" ? "language-active" : ""}
                             onClick={() => setLanguage("en")}
                         >
-                            English1
+                            English
                         </button>
 
                         <span>|</span>
