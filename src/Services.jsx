6473158f3d6e400e55447078ec3632064a1b45b1
Gaurav Bisht <gaurav.bisht@kellton.com>
2026-09-23 22:59:26 +0530

    fix

--- src/Services.jsx
+++ src/Services.jsx
@@ -105,9 +105,9 @@ export default function Services() {
                 }
             </section>
 
-            <a className="services_wa-float" id="floatWhatsapp" href="#" target="_blank" rel="noopener" aria-label="Chat on WhatsApp">
+            {/* <a className="services_wa-float" id="floatWhatsapp" href="#" target="_blank" rel="noopener" aria-label="Chat on WhatsApp">
                 <svg viewBox="0 0 24 24" fill="currentColor"><path d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91c0 1.75.46 3.45 1.32 4.95L2 22l5.29-1.39a9.9 9.9 0 0 0 4.75 1.21h.01c5.46 0 9.9-4.45 9.9-9.91 0-2.65-1.03-5.14-2.9-7.01A9.82 9.82 0 0 0 12.04 2Zm0 1.67c2.19 0 4.25.85 5.79 2.4a8.2 8.2 0 0 1 2.42 5.85c0 4.56-3.71 8.27-8.27 8.27a8.2 8.2 0 0 1-4.19-1.15l-.3-.18-3.14.82.84-3.06-.2-.32a8.18 8.18 0 0 1-1.26-4.38c0-4.56 3.71-8.25 8.31-8.25Zm-3.99 4.4c-.16 0-.42.06-.64.31-.22.25-.85.83-.85 2.02s.87 2.35.99 2.51c.12.16 1.7 2.72 4.2 3.7 2.08.82 2.5.66 2.95.62.45-.04 1.45-.59 1.65-1.16.2-.57.2-1.06.14-1.16-.06-.1-.22-.16-.46-.28-.24-.12-1.45-.72-1.68-.8-.22-.08-.39-.12-.55.12-.16.24-.63.8-.77.96-.14.16-.28.18-.52.06-.24-.12-1.02-.38-1.94-1.2-.72-.64-1.2-1.43-1.34-1.67-.14-.24-.02-.37.1-.49.11-.11.24-.28.36-.42.12-.14.16-.24.24-.4.08-.16.04-.3-.02-.42-.06-.12-.55-1.36-.76-1.85-.2-.48-.4-.42-.55-.42h-.47Z" /></svg>
-            </a>
+            </a> */}
             {
                 selectedService && (
                     <ServiceModal selectedService={selectedService} language={language} setSelectedService={setSelectedService} />
